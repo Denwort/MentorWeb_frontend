2124a762d95bb7810d2b34da2891836f3f9a0e15
PieroRozas <piero.rozas.2912@gmail.com>
2024-04-22 21:27:57 -0500

    1

---
+++ src/app/Header.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Header = () => {
+  return (
+    <header className="bg-white ">
+        <div className="flex flex-row p-8 gap-4">
+            <imge src="/MentorWebIco.png"></imge>
+            <h1>MentorWeb</h1>
+        </div>
+    </header>
+  );
+};
+
+export default Header;
---
+++ src/app/layout.tsx
@@ -0,0 +1,30 @@
+import type { Metadata } from "next";
+import { Inter } from "next/font/google";
+import Header from "./Header";
+import "./globals.css";
+
+const inter = Inter({ subsets: ["latin"] });
+
+
+
+const RootLayout = () => {
+  return (
+    <html lang="es">
+      <head>
+        <title>MentorWeb</title>
+      </head>
+
+      <body className="bg-customPink">
+
+        <Header />
+        
+        <main>
+          
+        </main>
+
+      </body>
+    </html>
+  );
+};
+
+export default RootLayout;
