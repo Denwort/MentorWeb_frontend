3f1fd9b5a333312d0844d69cf85125d006a84828
PieroRozas <piero.rozas.2912@gmail.com>
2024-04-22 22:07:46 -0500

    mi primera chamba

---
+++ src/app/Registro.js
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Registro = () => {
+  return (
+    <Registro className="">
+        <div className="flex flex-col ">
+
+            <h1>Login</h1>
+            
+            <div className="bg-white">
+                <form>
+                    
+                </form>
+            </div>
+
+        </div>
+    </Registro>
+  );
+};
+
+export default Registro;
--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import Header from "./Header";
+import Registro from "./Registro";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
