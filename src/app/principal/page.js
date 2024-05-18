59b2468ebaeb4460fcd5e847483daef9befcae39
ABGS2977 <20202977@aloe.ulima.edu.pe>
2024-05-18 18:10:26 -0500

    Insercion de data por Request al backend en pagina principal

--- src/app/principal/page.js
+++ src/app/principal/page.js
@@ -1,56 +1,87 @@
+'use client'
 import Image from "next/image";
 import ojo from "./../../../public/ojo.png";
 import persona from "./../../../public/persona.webp";
-import reservasData from '../../json/reservas.json';
+import React, { useState, useEffect } from 'react';
 
-export default function home() {
+const obtenerAsesoriasEstudiante = async (id) => {
+  try {
+    const response = await fetch('http://127.0.0.1:8000/asesorias_estudiante/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ "estudiante_id": id }),
+    });
+    if (!response.ok) {
+      throw new Error(`Error al obtener las asesorías del estudiante: ${response.statusText}`);
+    }
+    return await response.json();
+  } catch (error) {
+    console.error('Error al obtener las asesorías del estudiante:', error.message);
+    throw error;
+  }
+};
+
+const AsesoriasEstudianteCards = () => {
+  const [asesoriasData, setAsesoriasData] = useState([]);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const data = await obtenerAsesoriasEstudiante(144);
+        setAsesoriasData(data);
+      } catch (error) {
+        console.error('Error al obtener las asesorías del estudiante:', error.message);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <html lang="en">
-      <body className="bg-customPink">
+      <body className="bg-customPink" style={{ overflow: 'hidden' }}> {/*Overflow hidden para no poder desplazar la pagina*/}
         <div className="flex h-screen">
-
-          <nav class="w-1/6 h-screen flex flex-col space-y-4 pr-4 bg-white pt-4">
-              <a href="#" class="flex items-center py-2 px-4 bg-orange-500 text-white rounded-r-full">
-                <Image src={ojo} alt="Icono" className="h-6 w-6 mr-2"/>
-                Overview
-                </a>
-              <a href="/UserAsesores" class="flex items-center text-black py-2 px-4 hover:bg-orange-500 hover:text-white hover:rounded-r-full">
+          <nav className="w-1/6 h-screen flex flex-col space-y-4 pr-4 bg-white pt-4">
+            <a href="#" className="flex items-center py-2 px-4 bg-orange-500 text-white rounded-r-full">
+              <Image src={ojo} alt="Icono" className="h-6 w-6 mr-2"/>
+              Overview
+            </a>
+            <a href="/UserAsesores" className="flex items-center text-black py-2 px-4 hover:bg-orange-500 hover:text-white hover:rounded-r-full">
               <Image src={persona} alt="Icono" className="h-6 w-6 mr-2"/>
-                Asesores
-                </a>
+              Asesores
+            </a>
           </nav>          
 
           <div className="flex-1 p-4">
             <div className="container" style={{ padding: '20px' }}>
               <h2 className="subtitle" style={{ fontSize: '1.5rem', fontWeight: 'bold', marginBottom: '20px' }}>Asesorías próximas</h2>
-              <div className="cards" style={{ display: 'flex', flexWrap: 'wrap', gap: '20px',  }}>
-
-                {/*CARTAS DE LOS ASESORES*/}
-                {reservasData.map((reserva) => (
-                    <div key={reserva.id} className="card" style={{ width: 'calc(30% - 10px)', backgroundColor: "white", boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', borderRadius: '8px', overflow: 'hidden', position: 'relative', padding: '20px' }}>
-                        <div className="cardContent" style={{ display: 'flex', alignItems: 'center' }}>
-                            {/* Contenedor de la imagen circular */}
-                            <div className="imageContainer" style={{ position: 'relative', width: '100px', height: '100px', marginRight: '20px', background: 'linear-gradient(to right, rgba(255,255,255,0) 0%,rgba(255,255,255,1) 25%)' }}>
-                                <div className="circleBorder" style={{ position: 'absolute', width: '100%', height: '100%', borderRadius: '50%', border: '8px solid orange', clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0% 100%)' }}></div>
-                            </div>
-                            <div className="textContent" style={{ flex: '1' }}>
-                                <h3 className="cardTitle" style={{ fontSize: '1.2rem', fontWeight: 'bold', marginBottom: '10px' }}>{reserva.asesor}</h3>
-                                <h1 className="cardTitle" style={{ fontSize: '1.2rem', fontWeight: 'bold', marginBottom: '10px' }}>{reserva.curso}</h1>
-                                <p className="cardSubtitle" style={{ fontSize: '1rem', color: '#666', marginBottom: '5px' }}>{reserva.dia}</p>
-                                <p className="cardSubtitle" style={{ fontSize: '1rem', color: '#666', marginBottom: '5px' }}>{reserva.hora}</p>
-                            </div>
-                        </div>
-                        {/* Botón Ingresar */}
-                        <button className="ingresar-btn" style={{ backgroundColor: '#ff6440', borderRadius: '10px', padding: '8px', color: 'white', width: '90px', position: 'absolute', bottom: '10px', right: '10px' }}>Ingresar</button>
+              <div className="cards" style={{ display: 'flex', flexWrap: 'wrap', gap: '20px' }}>
+                {asesoriasData.map((asesoriaMAIN) => (
+                  <div key={asesoriaMAIN.id} className="card" style={{ width: 'calc(30% - 10px)', backgroundColor: "white", boxShadow: '0 4px 8px rgba(0, 0, 0, 0.1)', borderRadius: '8px', overflow: 'hidden', position: 'relative', padding: '20px' }}>
+                    <div className="cardContent" style={{ display: 'flex', alignItems: 'center' }}>
+                      <div className="imageContainer" style={{ position: 'relative', width: '100px', height: '100px', marginRight: '20px', background: 'linear-gradient(to right, rgba(255,255,255,0) 0%,rgba(255,255,255,1) 25%)' }}>
+                        <div className="circleBorder" style={{ position: 'absolute', width: '100%', height: '100%', borderRadius: '50%', border: '8px solid orange', clipPath: 'polygon(0 0, 100% 0, 100% 100%, 0% 100%)' }}></div>
+                      </div>
+                      <div className="textContent" style={{ flex: '1' }}>
+                        <h3 className="cardTitle" style={{ fontSize: '1.2rem', fontWeight: 'bold', marginBottom: '10px' }}>{asesoriaMAIN.asesoria.seccion.profesor.nombres}</h3>
+                        <h1 className="cardTitle" style={{ fontSize: '1.2rem', fontWeight: 'bold', marginBottom: '10px' }}>{asesoriaMAIN.asesoria.seccion.curso.nombre}</h1>
+                        <p className="cardSubtitle" style={{ fontSize: '1rem', color: '#666', marginBottom: '5px' }}>{asesoriaMAIN.asesoria.fecha_inicio}</p>
+                        <p className="cardSubtitle" style={{ fontSize: '1rem', color: '#666', marginBottom: '5px' }}>{asesoriaMAIN.asesoria.fecha_fin}</p>
+                        <p className="cardSubtitle" style={{ fontSize: '1rem', color: '#666', marginBottom: '5px' }}>ID General: {asesoriaMAIN.id}</p> {/*Le puse el id general pq parecia que se repetian, pero solo era que tenian la misma info xd*/}
+                      </div>
                     </div>
-                  ))}
-                
+                    <button className="ingresar-btn" style={{ backgroundColor: '#ff6440', borderRadius: '10px', padding: '8px', color: 'white', width: '90px', position: 'absolute', bottom: '10px', right: '10px' }}>Ingresar</button>
+                  </div>
+                ))}
               </div>
             </div>
           </div>
-          
         </div>
       </body>
     </html>
   );
-}
+};
+
+export default AsesoriasEstudianteCards;
