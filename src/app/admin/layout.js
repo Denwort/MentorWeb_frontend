6f85f5e21e60519a138af2dbd962e022d35e67d9
David <davidmm1107@hotmail.com>
2024-05-26 17:35:44 -0500

    location is not defined fixed

--- src/app/admin/layout.js
+++ src/app/admin/layout.js
@@ -26,7 +26,7 @@ export default function DashboardLayout({ children }) {
 
   return (
     <div className="flex h-screen">
-          <nav class="w-1/6 h-screen flex flex-col space-y-4 pr-4 pt-4 bg-[#fdfdfd]">
+          <nav class="min-w-52 h-screen flex flex-col space-y-4 pr-4 pt-4 bg-[#fdfdfd]">
             <p class="py-2 px-4 bg-orange-500 text-white rounded-r-full">Cargar Horarios</p>
             <button 
             className="cursor-pointer py-2 px-4 rounded-full hover:bg-red-400"
